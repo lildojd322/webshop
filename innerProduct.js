d7a9d6bd38e0e2ccbaa1a6ba7143b33768183f6c
Ivan Filippenko <sevcenkoivan395@gmail.com>
2025-10-15 20:00:17 +0300

    I made a non-final stylized version of the inner card

--- innerProduct.js
+++ innerProduct.js
@@ -7,17 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     document.title = product.name
     const productElement = document.createElement('div')
-    productElement.classList.add('productInnerInfo')
+    productElement.classList.add('product-inner')
     productElement.innerHTML = `
-            <img  src="${product.imageBase64}" width="140px" height="150px"
-                 class="product-icon" alt="${product.name}">
-            <div class="product-all-info">
-            <div class="product-price">${product.price} ₽</div>
-                <div class="product-name">${product.name}</div>
-                <div class="product-index">${product.index}</div>      
+            <img  src="${product.imageBase64}" width="440px" height="450px"
+                 class="product-inner-icon" alt="${product.name}">
+            <div class="product-inner-all-info">
+            <div class="product-inner-price">${product.price} ₽</div>
+                <div class="product-inner-name">${product.name}</div>
+                <div class="product-inner-index">${product.index}</div>      
             </div>
-              <div class="product-filter">${product.filter}</div>  
-            <button class="buy-product">в корзину</button>
+              <div class="product-inner-filter">${product.filter}</div>  
+            <button class="buy-inner-product">купить сейчас</button>
+            <button class="buy-inner-product">в корзину</button>
         `
     document.body.appendChild(productElement)
 
